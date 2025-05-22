72c9904c11e871a9443b4e8e09a48f821c92549f
CodeWithDevVishal <vishal.kore.2208@gmail.com>
2025-05-22 12:27:24 +0530

    Second Upload
    cors added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv/config")
+const cors = require("cors")
 
 const product = require("./route/productRoute")
 const user = require("./route/userRoute")
@@ -8,6 +9,7 @@ const category = require("./route/categoryRoute")
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.send("hii")
