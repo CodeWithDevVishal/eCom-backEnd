a0f64197b29438b59392c8ea146fba6b4a4869c9
CodeWithDevVishal <vishal.kore.2208@gmail.com>
2025-06-18 15:33:03 +0530

    first

---
+++ model/order.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const orderSchema = new mongoose.Schema({
+    userRef: {
+        type: mongoose.Schema.ObjectId
+    },
+    productRef: {
+        type: [mongoose.Schema.ObjectId]
+    },
+    total: {
+        type: Number
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model("order",orderSchema)
